016f76f18445254a0477b22b3cf75123df154a32
Abby Castillo <abby.machines@gmail.com>
2023-04-25 16:27:22 -0700

    fleshed out wave_01. expanded Planet class, expanded planets list, wrote handle_planets(), and moved the GET planet_id route next to handle_planet()

--- app/routes.py
+++ app/routes.py
@@ -1,21 +1,34 @@
 from flask import Blueprint, jsonify, abort, make_response
 
 class Planet:
-    def __init__(self, id, name, description):
+    def __init__(self, id, name, description, chemical_composition):
         self.id = id
         self.name = name
         self.description = description
+        self.composition = chemical_composition
 
 
 planets = [
-    Planet(1, "Mercury", "dark gray"), 
-    Planet(2, "Venus", "light yellow"),
-    Planet(3, "Earth", "blue and green")
+    Planet(1, "Mercury", "dark gray", "terrestrial"), 
+    Planet(2, "Venus", "light yellow", "terrestrial"),
+    Planet(3, "Earth", "blue and green", "terrestrial"),
+    Planet(4, "Mars", "red", "terrestrial"),
+    Planet(5, "Jupiter", "orange and brown", "gas giant")
 ]
 
 bp = Blueprint("planets", __name__, url_prefix="/planets")
 
-@bp.route("/<planet_id>", methods = ["GET"])
+@bp.route("", methods=["GET"])
+def handle_planets():
+    planets_response = []
+    for planet in planets:
+        planets_response.append({
+            "id": planet.id,
+            "name": planet.name,
+            "description": planet.description,
+            "chemical composition": planet.composition
+        })
+    return jsonify(planets_response)
 
 def validate_planet(planet_id):
     try:
@@ -33,9 +46,11 @@ def make_dict(planet):
     return dict(
                 id = planet.id,
                 name= planet.name,
-                description= planet.description
+                description= planet.description,
+                composition= planet.composition
             )
 
+@bp.route("/<planet_id>", methods = ["GET"])
 def handle_planet(planet_id):
     planet = validate_planet(planet_id)   
     return make_dict(planet)
