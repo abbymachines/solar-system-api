ae0411000993e7df87935c126ad7c8c5fbb550c6
Abby Castillo <abby.machines@gmail.com>
2023-05-04 15:56:21 -0700

    finished wave 6 up to test 3

---
+++ tests/test_routes.py
@@ -0,0 +1,52 @@
+from werkzeug.exceptions import HTTPException
+# from app.routes import validate_book
+import pytest
+
+def test_get_all_planets_with_no_records(client):
+    # Act
+    response = client.get("/planets")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == []
+
+def test_get_first_planet(client, two_saved_planets):
+    # Act
+    response = client.get("/planets/1")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == {
+        "id": 1,
+        "name": "first planet",
+        "description": "the first one",
+        "composition": "first"
+    }
+
+def test_get_first_planet_from_empty_database(client):
+    # Act
+    response = client.get("/planets/1")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 404
+    assert response_body == {"message": f"planet 1 not found"}
+
+def test_get_all_planets(client, two_saved_planets):
+    response = client.get("/planets")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == [
+        {"id": 1,
+        "name": "first planet",
+        "description": "the first one",
+        "composition": "first"},
+        {"id": 2,
+        "name": "second planet",
+        "description": "the second one",
+        "composition": "second"}
+    ]
